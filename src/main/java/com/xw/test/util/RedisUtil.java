5c46bf16d21ea2474998f0242fab3b182e1d45c0
xiongwei <944750471@qq.com>
2019-07-26 10:40:32 +0800

    新增分布式锁

--- src/main/java/com/xw/test/util/RedisUtil.java
+++ src/main/java/com/xw/test/util/RedisUtil.java
@@ -3,7 +3,9 @@ package com.xw.test.util;
 import com.alibaba.fastjson.JSON;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
-import redis.clients.jedis.*;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.*;
 
@@ -17,6 +19,14 @@ public class RedisUtil {
     private static final Logger log = Logger.getLogger(RedisUtil.class);
 
     private static JedisPool jedisPool = null;
+
+    private static final String LOCK_SUCCESS = "OK";
+
+    private static final String SET_IF_NOT_EXIST = "NX";
+
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+
+    private static final Long RELEASE_SUCCESS = 1L;
 
     /**
      * 初始化Redis连接池
@@ -658,4 +668,56 @@ public class RedisUtil {
     }
 
     //***********************Redis Set集合操作****************************end
+
+    //***********************Redis 分布式锁****************************start
+
+    /**
+     * 尝试获取分布式锁
+     *
+     * @param lockKey    锁
+     * @param requestId  请求标识
+     * @param expireTime 过期时间
+     * @return 是否获取成功
+     */
+    public static boolean tryGetDistributedLock(String lockKey, String requestId, int expireTime) {
+
+        try (Jedis jedis = getJedis()) {
+            String script = "if redis.call('setnx', KEYS[1], ARGV[1]) == 1 then redis.call('pexpire', KEYS[1], ARGV[2]) return 1 else return 0 end";
+            Object result = jedis.eval(script, Collections.singletonList(lockKey), Arrays.asList(requestId, String.valueOf(expireTime)));
+            //jedis 3.x不支持五个参数的set
+            //String result = jedis.set(lockKey, requestId, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
+
+            if (LOCK_SUCCESS.equals(result)) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    /**
+     * 释放分布式锁
+     *
+     * @param lockKey   锁
+     * @param requestId 请求标识
+     * @return 是否释放成功
+     */
+    public static boolean releaseDistributedLock(String lockKey, String requestId) {
+
+        try (Jedis jedis = getJedis()) {
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            Object result = jedis.eval(script, Collections.singletonList(lockKey), Collections.singletonList(requestId));
+
+            if (RELEASE_SUCCESS.equals(result)) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //***********************Redis 分布式锁****************************end
 }
